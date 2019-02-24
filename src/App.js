3f46a9e7d64ca5bb787476953c9e707e037dc7c4
Eric Hendrix <agentmrbig@gmail.com>
2019-02-24 17:29:03 -0500

    file errors cannot git add .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import Layout from '../src/components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <p>This is a test</p>
+        </Layout>
         
       </div>
     );
